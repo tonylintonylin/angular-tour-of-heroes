104975f350ebbb429e2c74580f81caa06426f099
Tony Lin <tonylinbusiness@gmail.com>
2020-11-24 12:23:08 -0500

    Cleaned up code

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from '../interfaces/hero';
+import { HEROES } from '../data/mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
@@ -15,7 +15,7 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find((hero) => hero.id === id));
   }
-  
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
